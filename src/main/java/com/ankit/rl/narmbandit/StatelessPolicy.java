087ba725a771b02cef417d4f5b9528a86710fff8
Sandeep Kumar <sandeep007734@gmail.com>
2017-09-25 18:57:44 +0530

    adding the leftover files

--- src/main/java/com/ankit/rl/narmbandit/StatelessPolicy.java
+++ src/main/java/com/ankit/rl/narmbandit/StatelessPolicy.java
@@ -1,5 +1,8 @@
 package com.ankit.rl.narmbandit;
 
+/**
+ * Simple Interface for different policies. Like Greedy, Epsilon Greedy and Softmax
+ */
 interface StatelessPolicy
 {
     public int getMachineId(double[] meanEstimates);
